350401f2c7a75712851e499da17a364ac5ded586
OctetularZ <taiabayomi15@gmail.com>
2023-11-21 21:32:56 +0000

    Removed the white navbar nonsense to normal sticky

--- index.js
+++ index.js
@@ -12,18 +12,49 @@ function raf(time) {
 requestAnimationFrame(raf)
 
 //GSAP scrolling
-let tl = gsap.timeline({
+
+let middleSection = gsap.timeline({
   scrollTrigger: {
     trigger: '.image-text-container',
-    start: '-140% 70%',
-    end: '-75% 40%',
+    start: '-180% 70%',
+    end: '200% 40%',
     toggleActions: 'restart restart none none',
     scrub: true,
     markers: true
   }
 })
 
-tl.to('.image-text-container', {
+middleSection.to('.image-text-container', {
   y: -500,
-  opacity: 1,
+  opacity: 1
 })
+
+middleSection.to('.image-text-container-2', {
+  y: -300,
+  opacity: 1
+})
+
+middleSection.to('.image-text-container-3', {
+  y: -350,
+  opacity: 1
+})
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction()
+};
+
+// Get the navbar
+let navbar = document.getElementById('outer-navi')
+
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
