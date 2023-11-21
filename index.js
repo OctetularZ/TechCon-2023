b4870831784eca171e2ab87848512b3064fba84e
OctetularZ <taiabayomi15@gmail.com>
2023-11-21 02:34:24 +0000

    Got gasp and lenus to work

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-import Lenis from '@studio-freight/lenis'
-import {
-  gsap
-} from "gsap";
-
-import {
-  ScrollTrigger
-} from "gsap/ScrollTrigger";
-
-
-gsap.registerPlugin(ScrollTrigger);
-
-
-//GSAP scrolling
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.image-text-container',
-    start: 'top center',
-    end: 'bottom center',
-    toggleActions: 'restart restart none none',
-    scrub: true,
-    markers: false
-  }
-})
-
-tl.to('.image-text-container', {
-  y: 400
-})
-
-
-// Lenis - Enables Smooth Scrolling
 const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
@@ -41,3 +10,19 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+//GSAP scrolling
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.image-text-container',
+    start: '-150% 80%',
+    end: '-75% center',
+    toggleActions: 'restart restart none none',
+    scrub: true,
+    markers: true
+  }
+})
+
+tl.to('.image-text-container', {
+  y: -500
+})
