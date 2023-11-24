88f959066babd4378debccb2092cef97151e2d21
OctetularZ <taiabayomi15@gmail.com>
2023-11-24 18:20:47 +0000

    middle of horizontal scroll animation

--- speakers/speakers.js
+++ speakers/speakers.js
@@ -12,3 +12,41 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+//gsap fake horizontal scroll effect
+
+let slider = document.querySelector('.slider')
+let sections = gsap.utils.toArray('.slider section')
+
+let tl = gsap.timeline({
+  defaults: {
+    ease: 'none'
+  },
+  scrollTrigger: {
+    trigger: slider,
+    pin: true,
+    scrub: 2,
+    end: () => "+=" + slider.offsetWidth,
+    markers: true
+  }
+})
+
+//Changes how far to the right you can scroll
+tl.to(slider, {
+  xPercent: -81
+})
+
+
+sections.forEach((stop, index) => {
+  tl.from(stop.querySelector('.content'), {
+    yPercent: -50,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: stop.querySelector('.content'),
+      start: 'left center',
+      end: 'cetner center',
+      containerAnimation: tl,
+      scrub: true
+    }
+  })
+})
